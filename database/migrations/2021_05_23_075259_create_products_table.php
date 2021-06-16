fff0c9b9ea46ebf0a3e9e06b7a413740b8338f5e
MohammedRaslan <mohamedraslan2282@yahoo.com>
2021-06-16 12:25:03 +0200

    new database structure

--- database/migrations/2021_05_23_075259_create_products_table.php
+++ database/migrations/2021_05_23_075259_create_products_table.php
@@ -17,17 +17,26 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('sku')->unique();
             $table->string('title');
+            $table->string('condition');
+            $table->string('brand');
             $table->tinyInteger('type');
+            $table->string('doll_size')->nullable();
+            $table->string('doll_gender')->nullable();
+            $table->boolean('modified_item')->default(0);
+            $table->boolean('domestic_product')->default(0);
+            $table->string('featured_refinements')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->string('upc')->nullable();
             $table->string('image');
             $table->longText('description');
             $table->longText('details')->nullable();
-            $table->string('condition');
-            $table->string('brand');
+            
             $table->tinyInteger('return_policy');
             $table->float('price');
             $table->boolean('best_offer');
             $table->boolean('draft')->default(0);
             $table->boolean('status')->default(1);
+            $table->integer('watchers')->default(0);
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
--- database/migrations/2021_05_23_085018_create_product_shippings_table.php
+++ database/migrations/2021_05_23_085018_create_product_shippings_table.php
@@ -16,11 +16,16 @@ class CreateProductShippingsTable extends Migration
         Schema::create('product_shippings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->longText('package_details')->nullable();
+            $table->string('width')->nullable();
+            $table->string('height')->nullable();
+            $table->string('length')->nullable();
+            $table->float('price');
             $table->integer('type');
             $table->json('usa')->nullable();
             $table->json('world_wide')->nullable();
             $table->float('weight')->nullable();
-            $table->string('duration');
+            $table->string('duration')->nullable();
             $table->timestamps();
         });
     }
