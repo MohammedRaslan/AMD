58172c2458be2aa080339d325f0f62b95ccae19f
mohamedelkamary5 <mohamedelkamary5@gmail.com>
2021-10-04 16:22:44 +0200

    fix shop details res

--- resources/js/components/ChatCenter/SideBarComponent.vue
+++ resources/js/components/ChatCenter/SideBarComponent.vue
@@ -2,13 +2,13 @@
     <div class="col-xl-2 col-md-12">
         <div :class="{ 'd-block': openSlideBar }" class="header-taps message heaer-tabs-yellow mb-lg-4">
             <div class="nav flex-column nav-pills m-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white" id="v-pills-home-tab" to="/notifications">Notifications</router-link>
+                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white" to="/notifications">Notifications</router-link>
                 <hr>
 
-                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white" id="v-pills-home-tab" to="/messages">Inbox</router-link>
+                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white" to="/messages">Inbox</router-link>
                 <hr>
 
-                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white" id="v-pills-home-tab" disabled to="/archive">Archive</router-link>
+                <router-link class="nav-link active-maroon pl-8 text-decoration-none text-white w-100" disabled to="/archive">Archive</router-link>
 
             </div>
         </div>
