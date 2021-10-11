f285bd918a48177b9ce558b5fb797cdeadebd226
mohamedelkamary5 <mohamedelkamary5@gmail.com>
2021-10-11 13:02:07 +0200

    return marketplace & add width to svg

--- resources/js/components/Navbar/DropdownComponent.vue
+++ resources/js/components/Navbar/DropdownComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="dropdown drop-1" :class="{ 'borderrad': dropCategories }" >
         <button class="btn btn-outline-secondary dropdown-toggle" @blur="blurDropCategories"  @click="clickDropCategories">
-            <span>Marketplace11</span>
-                <svg id="Icon_down_solid" xmlns="http://www.w3.org/2000/svg" width="20.833" height="20.833" viewBox="0 0 20.833 20.833"  :class="{ 'svg-tran': dropCategories }" >
+            <span>Marketplace</span>
+                <svg id="Icon_down_solid" xmlns="http://www.w3.org/2000/svg" style="width: 22px" width="20.833" height="20.833" viewBox="0 0 20.833 20.833"  :class="{ 'svg-tran': dropCategories }" >
                 <path id="Shape" d="M10.417,20.833A10.417,10.417,0,1,1,20.833,10.417,10.428,10.428,0,0,1,10.417,20.833ZM5.208,7.291A1.042,1.042,0,0,0,4.472,9.07L9.68,14.278a1.042,1.042,0,0,0,1.474,0L16.361,9.07A1.042,1.042,0,1,0,14.888,7.6l-4.472,4.472L5.945,7.6A1.035,1.035,0,0,0,5.208,7.291Z" fill="#fff"></path>
             </svg>
         </button>
